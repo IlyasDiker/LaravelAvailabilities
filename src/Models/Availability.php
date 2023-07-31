66d41fc7baa883be6f1ddb507b51103cccb0e474
IlyasDiker <ilyasbenhssine@gmail.com>
2023-07-31 22:16:20 +0100

    add availability factory

--- src/Models/Availability.php
+++ src/Models/Availability.php
@@ -4,11 +4,17 @@ namespace Ilyasdiker\LaravelAvailabilities\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Ilyasdiker\LaravelAvailabilities\Database\Factories\AvailabilityFactory;
 use Ilyasdiker\LaravelAvailabilities\Services\AvailabilityService;
 
 class Availability extends Model
 {
     use HasFactory;
+
+    protected static function newFactory()
+    {
+        return new AvailabilityFactory;
+    }
 
     protected $fillable = [
         'day_of_week',
